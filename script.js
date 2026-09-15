1dd56d254898c400071ac365d712a7df830cf9a2
bulaurimikhail <bulaurimikhail@gmail.com>
2026-09-15 15:06:20 +0400

    Add Codewars solutions

--- script.js
+++ script.js
@@ -569,7 +569,7 @@ Examples (a, b) --> output (explanation)
 (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
 Your function should only return a number, not the explanation about how you get that number.
 */
-function getSum(a, b) {
+/*function getSum(a, b) {
     let arr = [];
     const start = Math.min(a, b);
     const end = Math.max(a, b)
@@ -579,9 +579,29 @@ function getSum(a, b) {
     return arr.reduce((acc, el) => acc += el)
 }
 console.log(getSum(3, 1))
+*/
 
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////15.09.2026//////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
+//Friend or Foe? - kata/55b42574ff091733d900002f
+/*Description:
+Make a program that filters a list of strings and returns a list with only your friends name in it.
 
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Input = ["Ryan", "Kieran", "Jason", "Yous"]
+Output = ["Ryan", "Yous"]
+
+Input = ["Peter", "Stephen", "Joe"]
+Output = []
+Input strings will only contain letters.
+Note: keep the original order of the names in the output.
+*/
+function friend(friends) {
+
+}
 
 
 
