84ea9ddfecc7af12cc76e15e05007c3930f15969
bulaurimikhail <bulaurimikhail@gmail.com>
2026-09-16 15:28:09 +0400

    Add Codewars Solution

--- script.js
+++ script.js
@@ -726,7 +726,42 @@ Examples(Operator, value1, value2) --> output
 console.log(basicOp('/', 49, 7))
 */
 
-//
+//Abbreviate a Two Word Name - kata/57eadb7ecd143f4c9c0000a3
+/*Description:
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+/*function abbrevName(name) {
+    return name.split(' ').map((el) => el.substring(0, 1).toUpperCase()).join('.')
+}
+console.log(abbrevName('Sam Harris'));
+console.log(abbrevName('patrick feeney'))
+*/
+
+//Find the next perfect square! - kata/56269eb78ad2e4ced1000013
+/*Description:
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+Examples ( Input --> Output )
+121 --> 144
+625 --> 676
+114 --> -1  #  because 114 is not a perfect square
+*/
+function findNextSquare(sq) {
+    // Return the next square if sq is a perfect square, -1 otherwise
+    return -1;
+}
 
 
 
