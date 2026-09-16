7616d6cf35b2db5077364a8321ff287cbf08f1f5
bulaurimikhail <bulaurimikhail@gmail.com>
2026-09-16 15:03:57 +0400

    Add Codewars Solutions

--- script.js
+++ script.js
@@ -683,12 +683,47 @@ Example
 input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 */
-function openOrSenior(data) {
-    // ...
+/*function openOrSenior(data) {
+    let result = [];
+    for (const memeber of data) {
+        if (memeber[0] >= 55 && memeber[1] > 7) {
+            result.push('Senior');
+        } else {
+            result.push('Open');
+        }
+    }
+    return result
 }
+let a = [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]];
+console.log(openOrSenior(a));
+*/
+/*function openOrSenior2(data) {
+    return data.map(([age, handicap]) => (age >= 55 && handicap > 7) ? 'Senior' : 'Open')
+}
+console.log(openOrSenior2([[23, 5], [56, 12], [32, 20]]))
+*/
 
+//Basic Mathematical Operations - kata/57356c55867b9b7a60000bd7
+/*Description:
+Your task is to create a function that does four basic mathematical operations.
 
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
 
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+function basicOp(operation, value1, value2) {
+    let add = value1 + value2;
+    let sub = value1 - value2;
+    let mul = value1 * value2;
+    let div = value1 / value2;
+    return operation === '+' ? add : operation === '-' ? sub : operation === '*' ? mul : div
+}
+console.log(basicOp('/', 49, 7))
 
 
 
